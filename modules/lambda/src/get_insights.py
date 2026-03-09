eee25a9e3aab5cbc8ca0c43f8162316946337d00
corganfuzz <gruizcavero@uh.edu>
2026-03-08 19:28:07 -0500

    insights

--- modules/lambda/src/get_insights.py
+++ modules/lambda/src/get_insights.py
@@ -1,2 +1,6 @@
 def handler(event, context):
-    return {"statusCode": 200, "body": "Hello from get_insights"}
+    return {
+        "statusCode": 200,
+        "headers": {"Access-Control-Allow-Origin": "*", "Content-Type": "application/json"},
+        "body": json.dumps({"message": "Hello from get_insights"})
+    }
